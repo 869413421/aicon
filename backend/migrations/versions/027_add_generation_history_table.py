7fe966c2d054d45bbb0469ec490b6d922c7db38e
869413421 <13528685024@163.com>
2025-12-29 11:17:14 +0800

    fix:修复错误迁移文件名称

--- backend/migrations/versions/027_add_generation_history_table.py
+++ backend/migrations/versions/027_add_generation_history_table.py
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision = '027_add_generation_history_table'
-down_revision = '026_add_video_task_type'
+revision = '027'
+down_revision = '026'
 branch_labels = None
 depends_on = None
 
